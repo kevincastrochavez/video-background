93e00f5b0f37335dafb8c986cf5cfe9c886e09f4
kevincastrochavez <kevin.castro.chavez0@gmail.com>
2021-04-26 18:27:35 -0600

    created the footer component alogn with its styles and finished the website

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,12 +3,14 @@ import Hero from "../../components/Hero/Hero";
 import Cards from "../../components/Cards/Cards";
 
 import "../../App.css";
+import Footer from "../../components/Footer/Footer";
 
 function Home() {
   return (
     <>
       <Hero />
       <Cards />
+      <Footer />
     </>
   );
 }
